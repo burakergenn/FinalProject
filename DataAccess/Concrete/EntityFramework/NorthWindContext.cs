2546013ca81609bec1f1770414d4df1b945f3786
Burak Ergen <b.ergen@outlook.com.tr>
2021-02-03 23:47:56 +0300

    03.02.2021 ders sonu/EntityFramework eklendi.

---
+++ DataAccess/Concrete/EntityFramework/NorthWindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamaya yarar.
+    public class NorthWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=175.45.2.12"); -- normalde bunun gibi bir ip yazılarak db bağlanılır.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
