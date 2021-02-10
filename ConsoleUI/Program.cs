1ec3bfd7dc62a6c39193b9fa5f33d6682e6fa83e
Burak Ergen <b.ergen@outlook.com.tr>
2021-02-11 00:17:24 +0300

    11/2/21 core katmanında utilities eklendi ve business katmanında geliştirmeler yapıldı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,10 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
             //IoC
             //CategoryTest();
-            ProductTest();
+            
 
         }
 
@@ -29,10 +29,20 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
