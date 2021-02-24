edd8dfd52fb763c57a4b193333c883fb1d6491de
Burak Ergen <b.ergen@outlook.com.tr>
2021-02-25 00:09:47 +0300

    25/02/2021 ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,9 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün Eklendi";
         public static string ProducNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
-        public static string ProductsListed = "Ürünler Listelendi"; 
+        public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 15 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
